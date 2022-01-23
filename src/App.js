796e6f663dbc1226cd3ef87edba7f14ffd2c5b56
aalrayes <a.alrayes.404@gmail.com>
2022-01-23 22:53:03 +0300

    almost done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
-
+import BoostLinks from "./components/BoostLinks";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import LinkShortner from "./components/LinkShortner";
+import Nav from "./components/Nav";
+import Statistics from "./components/Statistics";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <main className=" w-desktop mx-auto font-poppins  ">
+        <Nav />
+        <Hero />
+        <LinkShortner />
+      </main>
+      <Statistics />
+      <BoostLinks />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: "hsl(180, 66%, 49%)",
+        darkViolet: "hsl(257, 27%, 26%)",
+        red: "hsl(0, 87%, 67%)",
+        gray: "hsl(0, 0%, 75%)",
+        grayishViolet: "hsl(257, 7%, 63%)",
+        veryDarkBlue: "hsl(255, 11%, 22%)",
+        veryDarkViolet: "hsl(260, 8%, 14%)",
+      },
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+      },
+      maxWidth: {
+        desktop: "1440px",
+        mobile: "375px",
+      },
+      width: {
+        desktop: "1440px",
+      },
+      fontSize: {
+        body: "18px",
+      },
+      backgroundImage: {
+        "link-pattern-desktop":
+          "url('/src/assets/images/bg-shorten-desktop.svg')",
+        "link-pattern-mobile":
+          "url('/src/assets/images/bg-shorten-mobile.svg')",
+      },
+    },
   },
   plugins: [],
 };
