6a571c445a96d0323c9c8e3ac79099cf1b1b5090
aalrayes <a.alrayes.404@gmail.com>
2022-06-02 15:11:48 +0300

    small patch

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import {IoClose} from "react-icons/io5"
+import React from "react";
+import { IoClose } from "react-icons/io5";
 
-const MobileNav = ({handleToggle}) => {
-    return (
-      <div className=" h-64  w-full bg-violet-900
-            rounded-2xl absolute z-50 right-0">
-         <IoClose onClick={handleToggle} className=' text-4xl mt-2 mr-3 ml-auto text-white cursor-pointer'/>      
-     <ul className="my-1 justify-between text-center mr-3 flex flex-col font-bold text-white text-xl">
-          <li>
-            <a className="hover:text-veryDarkBlue m-5" href="/">
-              Features
-            </a>
-          </li>
-          <li>
-            <a className="hover:text-veryDarkBlue m-5" href="/">
-              Pricing
-            </a>
-          </li>
-          <li>
-            <a className="hover:text-veryDarkBlue m-5" href="/">
-              Resources
-            </a>
-          </li>
-        </ul>
-
-        </div>
-    );
-}
+const MobileNav = ({ handleToggle }) => {
+  return (
+    <div
+      className=" absolute  right-0 z-50
+            block h-64 w-full rounded-2xl bg-violet-900 md:hidden"
+    >
+      <IoClose
+        onClick={handleToggle}
+        className=" mt-2 mr-3 ml-auto cursor-pointer text-4xl text-white"
+      />
+      <ul className="my-1 mr-3 flex flex-col justify-between text-center text-xl font-bold text-white">
+        <li>
+          <a className="m-5 hover:text-veryDarkBlue" href="/">
+            Features
+          </a>
+        </li>
+        <li>
+          <a className="m-5 hover:text-veryDarkBlue" href="/">
+            Pricing
+          </a>
+        </li>
+        <li>
+          <a className="m-5 hover:text-veryDarkBlue" href="/">
+            Resources
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default MobileNav;
--- src/components/navLinks.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Navlinks = () => {
-  return <div></div>;
-};
-
-export default Navlinks;
